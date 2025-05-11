09c090d212fc65e0d647989eb4c4a7ad48aff933
ashim-sharma-console <ashimsharma49@gmail.com>
2025-05-12 00:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-// Complete the js code
-function Car(make, model) {}
+ function Car(make, model) {
+      this.make = make;
+      this.model = model;
+    }
 
-function SportsCar(make, model, topSpeed) {}
+    Car.prototype.getMakeModel = function () {
+      return `${this.make} ${this.model}`;
+    };
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+    // SportsCar Constructor Function
+    function SportsCar(make, model, topSpeed) {
+      Car.call(this, make, model); // Inherit Car properties
+      this.topSpeed = topSpeed;
+    }
+
+    // Inherit Car prototype
+    SportsCar.prototype = Object.create(Car.prototype);
+    SportsCar.prototype.constructor = SportsCar;
+
+    SportsCar.prototype.getTopSpeed = function () {
+      return this.topSpeed;
+    };
+
+    // Function to create a car and show results
+    function createCar() {
+      const make = document.getElementById('make').value;
+      const model = document.getElementById('model').value;
+      const topSpeed = Number(document.getElementById('topSpeed').value);
+
+      const car = new SportsCar(make, model, topSpeed);
+
+      const output = `
+        <strong>Make & Model:</strong> ${car.getMakeModel()}<br>
+        <strong>Top Speed:</strong> ${car.getTopSpeed()} mph
+      `;
+
+      document.getElementById('output').innerHTML = output;
+    }
